a551cb9f518603a7a086996a1d632ffcd9590405
Elias Hefti <elias.hefti@gmail.com>
2025-06-10 19:22:46 +0200

    20250610

--- MaxTwo_Activity_Screening/screenActivity.m
+++ MaxTwo_Activity_Screening/screenActivity.m
@@ -1,17 +1,13 @@
-% --- Plot Amplitude Overview ---
-
 function screenActivity(dataPath, saveDir)
+    
+    addpath(genpath('functions'));
 
     fprintf('Screening Activity');
     activityScanData = loadActivityScanData(dataPath);
 
 
     % 2. Create and Save Plot
-    fileName = "Activity Scan.png"
+    fileName = "Activity Scan.png";
     plotWellActivityOverview(activityScanData, saveDir, fileName);
 
 end
-
-
-cd (fullfile(matlabroot,'extern','engines','python'))
-system('python setup.py install')
